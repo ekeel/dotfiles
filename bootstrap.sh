2ed65ce018ef1768b3b36cace2b81f61dee5ff17
Eric Keeling <ekeel@ekeel.net>
2023-08-03 12:59:42 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install -y wget apt-transport-https software-properties-common
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
